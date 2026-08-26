1fcabb1bbf2bde026476741cdb840b65110286a9
romanageev538-cell <romanageev538@gmail.com>
2026-08-26 18:01:35 +0300

    Third commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,24 +2,27 @@ import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import model.LoginModel;
 import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
 import io.restassured.response.Response;
 
-import static org.apache.http.HttpStatus.*;
 import static org.hamcrest.Matchers.*;
 
 public class LoginTest extends BaseApiTest {
+
+    @Before
+    public void prepareUser() {
+        createUser(); // создаём пользователя перед каждым тестом
+    }
 
     @Test
     @DisplayName("Вход под существующим пользователем")
     @Description("Проверка успешного логина с верными email и паролем")
     public void testLoginExistingUser() {
-        createUser();
-
         LoginModel login = new LoginModel(email, password);
         Response response = loginSteps.login(login);
         response.then()
-                .statusCode(SC_OK)
+                .statusCode(HttpStatus.SC_OK)
                 .body("success", is(true))
                 .body("user.email", equalTo(email))
                 .body("user.name", equalTo(name))
@@ -27,15 +30,26 @@ public class LoginTest extends BaseApiTest {
     }
 
     @Test
-    @DisplayName("Вход с неверным логином и паролем")
-    @Description("Проверка ошибки при неверных учётных данных")
-    public void testLoginInvalidCredentials() {
-        createUser();
+    @DisplayName("Вход с неверным email")
+    @Description("Проверка ошибки при входе с несуществующим email")
+    public void testLoginInvalidEmail() {
+        String wrongEmail = "wrong_" + email;
+        LoginModel login = new LoginModel(wrongEmail, password);
+        Response response = loginSteps.login(login);
+        response.then()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
+                .body("success", is(false))
+                .body("message", equalTo("email or password are incorrect"));
+    }
 
+    @Test
+    @DisplayName("Вход с неверным паролем")
+    @Description("Проверка ошибки при входе с неправильным паролем")
+    public void testLoginInvalidPassword() {
         LoginModel login = new LoginModel(email, "wrongPassword");
         Response response = loginSteps.login(login);
         response.then()
-                .statusCode(SC_UNAUTHORIZED)
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
                 .body("success", is(false))
                 .body("message", equalTo("email or password are incorrect"));
     }
